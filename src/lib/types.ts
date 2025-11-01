b73a09285d80d2a6ce288cba94ba115ea8a86233
stalkiq <almoreau1@outlook.com>
2025-11-01 18:07:54 -0400

    Remove Category, Facebook URL, and Facebook Last Post columns

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,9 +3,6 @@ export type BusinessData = {
   businessName: string;
   address: string;
   phone: string;
-  category: string;
-  facebookUrl: string;
-  facebookLastPost: string;
   instagramUrl: string;
   instagramPresent: string;
   website: string;
